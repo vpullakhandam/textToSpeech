944ddcc48672ba22a577eb80911065296ac4505b
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-02-14 14:54:57 +0530

    updating the code

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-let speech = new SpeechSynthesisUtterance();
+// let speech = new SpeechSynthesisUtterance();
 
-// create a list of option of voices
-let voices = [];
-let voiceSelect = document.querySelector("select");
-window.speechSynthesis.onvoiceschanged = listOfVoices;
+// // create a list of option of voices
+// let voices = [];
+// let voiceSelect = document.querySelector("select");
+// window.speechSynthesis.onvoiceschanged = listOfVoices;
 
-// changes the voice when we click on the option in the dropdown from select
-voiceSelect.addEventListener("change", changeVoice);
+// // changes the voice when we click on the option in the dropdown from select
+// voiceSelect.addEventListener("change", changeVoice);
 
-// clicking the button invokes playing the audio
-document.querySelector("button").addEventListener("click", play);
+// // clicking the button invokes playing the audio
+// document.querySelector("button").addEventListener("click", play);
 
-function play() {
-  document.querySelector("button").style.backgroundColor = "#0f172a";
-  speech.text = document.querySelector("textarea").value;
-  window.speechSynthesis.speak(speech);
-  speech.onend = function () {
-    finished();
-  };
-}
-function finished() {
-  document.querySelector("button").style.backgroundColor = "#64748b";
-}
-function listOfVoices() {
-  voices = window.speechSynthesis.getVoices();
-  speech.voice = voices[0]; // default is the first voice on the system.
-  const i = 1;
-  voices.forEach(
-    (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
-  );
-}
-function changeVoice() {
-  speech.voice = voices[voiceSelect.value];
-}
+// function play() {
+//   document.querySelector("button").style.backgroundColor = "#0f172a";
+//   speech.text = document.querySelector("textarea").value;
+//   window.speechSynthesis.speak(speech);
+//   speech.onend = function () {
+//     finished();
+//   };
+// }
+// function finished() {
+//   document.querySelector("button").style.backgroundColor = "#64748b";
+// }
+// function listOfVoices() {
+//   voices = window.speechSynthesis.getVoices();
+//   speech.voice = voices[0]; // default is the first voice on the system.
+//   const i = 1;
+//   voices.forEach(
+//     (voice, i) => (voiceSelect.options[i] = new Option(voice.name, i))
+//   );
+// }
+// function changeVoice() {
+//   speech.voice = voices[voiceSelect.value];
+// }
